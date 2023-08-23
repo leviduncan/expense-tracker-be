9a54a3b7c98d446ae7e7aa70fc194a01ceb6f39d
darrin@darrinduncan.com <darrin@darrinduncan.com>
2023-08-22 21:30:09 -0400

    fix: get transactions

--- routes/api/transactionRoutes.js
+++ routes/api/transactionRoutes.js
@@ -16,7 +16,7 @@ router.post('/add', (req, res) => {
 // Get all transactions
 router.get('/', (req, res) => {
     Transaction.find()
-                .then(transactions => res.json(transaction))
+                .then(transactions => res.json(transactions))
                 .catch(err => res.status(400).json(err))
 })
 
